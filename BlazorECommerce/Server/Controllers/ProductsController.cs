a583e329250fafc68aa532f2a7e8b1a72ca15b19
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-01-10 00:40:17 +0100

    começando a criar controllers

---
+++ BlazorECommerce/Server/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorECommerce.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+
+}
